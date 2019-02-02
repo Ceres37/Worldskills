bbf8db4993ea48d68e3c6b5b960b7feb6cf65e85
Ceres37 <sever.gil@mail.ru>
2019-02-02 23:03:05 +0800

    Add project files.

---
+++ №2/Test.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace _2
+{
+    public partial class Test : Form
+    {
+        const string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Ceres\source\repos\№2\№2\Database1.mdf;Integrated Security=True";
+        public Test()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, string> db = new Dictionary<string, string>();
+
+            SqlCommand getUsersCredCmd = new SqlCommand("SELECT [Login], [Password] FROM [Users]", SqlConnection);
+
+            SqlDataReader sqlReader = null;
+
+            try
+            {
+                sqlReader = await getUsersCredCmd.ExecuteReaderAsync();
+
+                while (await sqlReader.ReadAsync())
+                {
+                    db.Add(Convert.ToString(sqlReader["Login"]), Convert.ToString(sqlReader["Password"]));
+                }
+            }
+            catch { }
+            finally
+            {
+                if (sqlReader != null)
+                    sqlReader.Close();
+            }
+
+            if (textBox2.Text == db[textBox1.Text])
+            {
+                this.Hide();
+                Form2 f2 = new Form2();
+                f2.ShowDialog();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+                this.Close();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
